ae0059f9bd3f30f2f1fc1101a66e9b8028c39be7
Ishaan Agarwal <ishaan.agarwal.cer21@iitbhu.ac.in>
2024-09-16 20:49:32 +0530

    chore: Update sign-in page layout and add chat link
    Updated the layout of the sign-in page to include a chat link. The chat link is displayed as a button with a chevron icon and the text "Open Chat". Clicking on the link redirects to the Hangout website. This change improves the user experience by providing easy access to the chat feature while signing in.

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,21 @@
 import { SignIn } from "@clerk/nextjs";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function SignInPage() {
   return (
-    <main className="flex h-screen w-full items-center justify-center">
-      <SignIn />
+    <main className="flex flex-col h-screen w-full items-center justify-center">
+      <div className="text-white w-[400px] flex bg-[#1B1E2C] mb-[-15px] py-4 rounded-xl">
+        <Link
+          href="https://hangout.ishaanagarwal.xyz/"
+          className="flex mx-4 text-[#0B7AF9] text-xs items-center"
+        >
+          <ChevronLeft /> Open Chat
+        </Link>
+      </div>
+      <div className="">
+        <SignIn />
+      </div>
     </main>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,7 +11,8 @@ export default function Sidebar() {
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
-          const isActive = pathname === link.route || pathname === link.route + "/";
+          const isActive =
+            pathname === link.route || pathname === link.route + "/";
           return (
             <Link
               href={link.route}
@@ -34,6 +35,20 @@ export default function Sidebar() {
           );
         })}
       </div>
+      {/* <p>Check</p> */}
+      <Link
+        href={process.env.NEXT_PUBLIC_HANGOUT_URL!}
+        // key={Hangout}
+        className={cn("flex gap-4 items-center p-4 rounded-lg justify-start")}
+      >
+        <Image
+          src="/icons/message-square.svg"
+          width={24}
+          height={24}
+          alt="Hangout"
+        />
+        <p className="text-lg font-semibold max-lg:hidden">Hangout</p>
+      </Link>
     </section>
   );
 }
